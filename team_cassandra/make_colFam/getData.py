a06523e73da48feee2fd49205e389978163c5fea
Chad Tolleson <tolleson@pdx.edu>
2019-11-10 19:22:50 -0800

    updated python code

--- team_cassandra/make_colFam/getData.py
+++ team_cassandra/make_colFam/getData.py
@@ -10,8 +10,8 @@ col_fam = ColumnFamily(pool, 'cassandraGroup')
 name = col_fam.get('Chad')
 print(name)
 
-name = col_fam.get('Will')
-print(name)
+# name = col_fam.get('Will')
+# print(name)
 
-name = col_fam.get('Tom')
-print(name)
+# name = col_fam.get('Tom')
+# print(name)
