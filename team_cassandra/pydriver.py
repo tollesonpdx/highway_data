c44e96117344d9f23c9eb829f27d0177ef843038
Chad Tolleson <tolleson@pdx.edu>
2019-11-11 11:14:25 -0800

    removed old and duplicative python files

--- team_cassandra/pydriver.py
+++
@@ -1,7 +0,0 @@
-from cassandra.cluster import Cluster
-cluster = Cluster()
-session = cluster.connect('testcass')
-# session.set_keyspace('testcass')
-
-stat = session.execute('describe testcass')
-print(stat)
