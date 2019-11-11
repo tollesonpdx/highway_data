ce212244ed5a2d02ea1efbe1964ccad899fd6514
Chad Tolleson <tolleson@pdx.edu>
2019-11-11 11:38:42 -0800

    updated chads test python

--- team_cassandra/load_highway_data/load_chad_test.py
+++ team_cassandra/load_highway_data/load_chad_test.py
@@ -23,16 +23,9 @@ with open(detectorFile, 'rU') as fin:
 
 with open(loopFile, 'rU') as fin:
     # loopDatain = csv.DictReader(fin, delimiter=',')
-    cin = csv.DictReader(fin)
-    it = 0
-    for row in cin:
-        print(it)
-        it = it + 1
-        print(row)
-    #make each row in csv file into dictionary and catches them in list
-    #leads to mem overflow
-    # loopData= [row for row in cin]
-#print(loopDatain.values())
+    loopin = csv.DictReader(fin)
+    # for row in loopin:
+        # print(row)
 
 
 
@@ -43,14 +36,21 @@ for station in stationData:
     station_col_fam.insert(station['stationid'],
             {'highwayid': station['highwayid'], 'milepost':station['milepost'], 'locationtext':station['locationtext'], 'upstream':station['upstream'],'downstream':station['downstream'], 'stationclass':station['stationclass'], 'numberlanes':station['numberlanes'], 'latlon': station['latlon'], 'length':station['length']})
 print('getting info for station id 1098')
-print(station_col_fam.get('1098'))
+print(station_col_fam.get('1098')+'')
 
 detector_col_fam = ColumnFamily(pool, 'chaddetectors')
 for det in detectorData:
     detector_col_fam.insert(det['detectorid'],
             {'highwayid': det['highwayid'], 'milepost':det['milepost'], 'locationtext':det['locationtext'], 'detectorclass':det['detectorclass'],'lanenumber':det['lanenumber'], 'stationid':det['stationid']})
 print('getting info for detector id 1810')
-print(detector_col_fam.get('1810'))
+print(detector_col_fam.get('1810')+'')
+
+loop_col_fam = ColumnFamily(pool, 'chadloops')
+for row in loopin:
+    loop_col_fam.insert((row['detectorid'] + ' - ' + row['starttime']),
+            {'detectorid': row['detectorid'], 'starttime':row['starttime'], 'volume':row['volume'], 'speed':row['speed'],'occupancy':row['occupancy'], 'status':row['status'], 'dqflags':row['dqflags']})
+print('getting info for detector & starttime 1345 - 9/15/2011  12:04:00 AM')
+print(loop_col_fam.get('1345 - 9/15/2011  12:04:00 AM')+'')
 
 print('all done')
 # pool.close()
