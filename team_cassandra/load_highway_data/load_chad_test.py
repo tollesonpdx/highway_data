c5017c8891a765d1a01f2c3b7d5ce6abb047e686
Chad Tolleson <tolleson@pdx.edu>
2019-11-10 23:02:08 -0800

    updated chad test

--- team_cassandra/load_highway_data/load_chad_test.py
+++ team_cassandra/load_highway_data/load_chad_test.py
@@ -17,7 +17,7 @@ with open(detectorFile, 'rU') as fin:
 
 with open(loopFile, 'rU') as fin:
     loopDatain = csv.DictReader(fin, delimiter=',')
-    loopDatain = csv.DictReader(fin)
+    # loopDatain = csv.DictReader(fin)
     
     #make each row in csv file into dictionary and catches them in list
     #leads to mem overflow
