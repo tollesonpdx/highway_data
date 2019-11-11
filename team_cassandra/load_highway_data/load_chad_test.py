3098bff4792b43481d847b4ad69426ef8bf76cfb
Chad Tolleson <tolleson@pdx.edu>
2019-11-11 10:12:12 -0800

    updated chad test py

--- team_cassandra/load_highway_data/load_chad_test.py
+++ team_cassandra/load_highway_data/load_chad_test.py
@@ -34,11 +34,11 @@ pool = ConnectionPool('highwayData', ['localhost:9160'])
 
 col_fam = ColumnFamily(pool, 'chadstation')
 
-col_fam.insert(int(stationData['stationid']), {'name': {'last': 'Mass'}})
+# col_fam.insert(stationData['stationid'], {'name': {'last': 'Mass'}})
 
-# for station in stationData:
-#     col_fam.insert(station['stationid'],
-#             {'highwayid': station['highwayid'], 'milepost':station['milepost'], 'locationtext':station['locationtext'], 'upstream':station['upstream'],'downstream':station['downstream'], 'stationclass':station['stationclass'], 'numberlanes':station['numberlanes', 'latlon': station['latlon'], 'length':station['length']})
+for station in stationData:
+    col_fam.insert(station['stationid'],
+            {'highwayid': station['highwayid'], 'milepost':station['milepost'], 'locationtext':station['locationtext'], 'upstream':station['upstream'],'downstream':station['downstream'], 'stationclass':station['stationclass'], 'numberlanes':station['numberlanes', 'latlon': station['latlon'], 'length':station['length']})
 
 
 
