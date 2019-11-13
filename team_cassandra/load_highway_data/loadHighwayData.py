9485e4d9cc636adf700d461e63391bd4a9d5d80b
ejdeposit <ejdeposit@gmail.com>
2019-11-13 05:09:39 +0000

    finished program to load all data

--- team_cassandra/load_highway_data/loadHighwayData.py
+++ team_cassandra/load_highway_data/loadHighwayData.py
@@ -16,21 +16,16 @@ with open(detectorFile, 'rU') as fin:
     detectorData= [row for row in cin]
 
 
-with open(loopFile, 'rU') as fin:
-    loopDatain = csv.DictReader(fin, delimiter=',')
-    #for row in loopDatain:
-    #    pass
-
-print(stationData[0])
-print(detectorData[0])
 
 pool = ConnectionPool('highwayData', ['localhost:9160'])
-#pool = ConnectionPool('highwayStand', ['localhost:9160'])
 
+#change to not super column
 col_fam = ColumnFamily(pool, 'stationid')
 
-#col_fam.insert(stationData['stationid'], {'name': {'last': 'Mass'}})
+col_fam_detectors = ColumnFamily(pool, 'detectors')
 
+
+#change this so not super column
 for station in stationData:
     stationid=station['stationid']
     highwayid= station['highwayid']
@@ -42,5 +37,24 @@ for station in stationData:
     numberlanes= station['numberlanes']
     latlon= station['latlon']
     length= station['length']
+    
+    col_fam.insert(stationid,
+            {'highwayid': highwayid, 'milepost': milepost, 'locationtext': locationtext, 'upstream': upstream, 'downstream': downstream, 'stationclass': stationclass, 'numberlanes': numberlanes, 'latlon': latlon, 'length': length})
 
-    col_fam.insert(stationid, {'stationInfo': {'highwayid': highwayid, 'milepost': milepost, 'locationtext': locationtext, 'upstream': upstream, 'downstream': downstream, 'stationclass': stationclass, 'numberlanes': numberlanes, 'latlon': latlon, 'length': length}, 'detectors':{}  })
+
+for detector in detectorData:
+    detectorid=detector['detectorid']
+
+    col_fam_detectors.insert(detectorid,
+            { 'detectorInfo':{'highwayid': detector['highwayid'], 'milepost':detector['milepost'], 'locationtext':detector['locationtext'], 'detectorclass':detector['detectorclass'], 'lanenumber':detector['lanenumber']}})
+
+
+    with open(loopFile, 'rU') as fin:
+        loopDatain = csv.DictReader(fin, delimiter=',')
+        for row in loopDatain:
+
+            if row['detectorid'] == detectorid:
+
+                col_fam_detectors.insert(detectorid,
+                        { row['starttime']: {'volume': row['volume'], 'speed': row['speed'], 'occupancy': row['occupancy'], 'status':row['status'], 'dqflags':row['dqflags']} })
+
--- team_cassandra/load_highway_data/printRows.py
+++ team_cassandra/load_highway_data/printRows.py
@@ -6,17 +6,38 @@ from pycassa.columnfamily import ColumnFamily
 pool = ConnectionPool('highwayData', ['localhost:9160'])
 
 col_fam = ColumnFamily(pool, 'stationid')
+detector_col_fam = ColumnFamily(pool, 'detectors')
 
 
+detectorFile='/home/edeposit/ProjectData-Cloud2015/freeway_detectors.csv'
+loopFile='/home/edeposit/ProjectData-Cloud2015/freeway_loopdata.csv'
 stationFile='/home/edeposit/ProjectData-Cloud2015/freeway_stations.csv'
+
 with open(stationFile, 'rU') as fin:
     cin = csv.DictReader(fin)
     stationData= [row for row in cin]
+
+with open(detectorFile, 'rU') as fin:
+    cin = csv.DictReader(fin)
+    detectorData= [row for row in cin]
 
 stations=[]
 for station in stationData:
     stations.append(station['stationid'])
 
+detectors=[]
+for detector in detectorData:
+    detectors.append(detector['detectorid'])
+print(detectors)
+
 for stationid in stations:
     stationInfo= col_fam.get(stationid)
+    print('Station ', stationid)
     print(stationInfo)
+    print('')
+    
+for detectorid in detectors:
+    detectorInfo= detector_col_fam.get(detectorid)
+    print('detector ', detectorid )
+    print(detectorInfo)
+    print('')
