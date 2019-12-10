ce9e5f8fa331e2241086794159f4c57126e49a2a
Chad Tolleson <tolleson@pdx.edu>
2019-12-10 08:00:01 -0800

    added testing files

---
+++ team_cassandra/load_highway_data/dropHighwayDataColFams.py
@@ -0,0 +1,22 @@
+import pycassa
+from pycassa.columnfamily import ColumnFamily
+from pycassa.system_manager import *
+
+sys = SystemManager('10.138.0.5:9160')
+
+print('dropping old tables')
+
+####################################################
+####### BE CAREFUL UNCOMMENTING THESE LINES ########
+####################################################
+
+### sys.drop_column_family('highwaydata', 'stations')
+sys.drop_column_family('highwaydata', 'detectors')
+### sys.drop_column_family('highwaydata', 'loopdata')
+
+
+print('dropped them like hot potatos')
+
+sys.close()
+
+print('all done')
