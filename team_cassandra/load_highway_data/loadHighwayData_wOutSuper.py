c110e956666a3744c9ba65be5e777d36b930b095
Chad Tolleson <tolleson@pdx.edu>
2019-12-10 08:33:25 -0800

    added testing files

--- team_cassandra/load_highway_data/loadHighwayData_wOutSuper.py
+++ team_cassandra/load_highway_data/loadHighwayData_wOutSuper.py
@@ -6,7 +6,7 @@ import csv
 import time
 
 sys = SystemManager('10.138.0.5:9160')
-pool = ConnectionPool('highwaydata', ['10.138.0.5', '10.138.0.4', '10.138.0.3'], use_threadlocal=False, pool_size=3)
+pool = ConnectionPool('highwaydata', ['10.138.0.5', '10.138.0.4', '10.138.0.3'], use_threadlocal=False, pool_size=9)
 
 
 stationFile = '/home/highway_data/csv_fies/ProjectData-Cloud2015/freeway_stations.csv'
