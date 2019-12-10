02be420505793962721016bb17e6c3518af33ace
Chad Tolleson <tolleson@pdx.edu>
2019-12-10 15:47:27 -0800

    added times file

--- team_cassandra/queries/query3.py
+++ team_cassandra/queries/query3.py
@@ -14,8 +14,14 @@ print('query 3 - getting data from Cassandra')
 
 station_col_fam = ColumnFamily(pool, 'stations')
 detector_col_fam = ColumnFamily(pool, 'detectors')
-loop_col_fam = ColumnFamily(pool, 'loopdata_new')  #loopdata_new
+loop_col_fam = ColumnFamily(pool, 'loopdata_new')
+
 timesFile = '/home/highway_data/csv_fies/2011_09_22_times.txt'
+
+query3_results = open('query3_results.txt', 'a')
+query3_results.write('\nquery run time: ')
+query3_results.write(str(time.ctime(int(time.time()))))
+query3_results.write('\n')
 
 fosterNBID = '' 
 fosterNBLength = 0.0 #length of station NB Foster
@@ -101,7 +107,8 @@ for datum in loops:
 extract_end_time = time.time()
 print("it took %s seconds to get data from Cassandra for query 3" % (extract_end_time - query3_start_time))
 print('NB Foster station length: ' + str(fosterNBLength) + ' miles')
-
+query3_results.write("It took %s seconds to get data from Cassandra for query 3\n" % (extract_end_time - query3_start_time))
+query3_results.write('NB Foster station length: ' + str(fosterNBLength) + ' miles\n')
 
 # analyis starts here
 begtime = datetime.datetime(2011, 9, 22, 0, 0, 0)
@@ -118,12 +125,15 @@ while (endtime < datetime.datetime(2011, 9, 23, 0, 0, 0)):
         avgspeed = (totalspeed * 1.0) / (totalvolume * 1.0)
         avgtraveltime = fosterNBLength / avgspeed * 3600
         print('average travel time from ' + begtime.strftime("%Y-%m-%d %H:%M:%S") + ' to ' + endtime.strftime("%Y-%m-%d %H:%M:%S") + ' is ' + str(avgtraveltime) + ' seconds')
+        query3_results.write('average travel time from ' + begtime.strftime("%Y-%m-%d %H:%M:%S") + ' to ' + endtime.strftime("%Y-%m-%d %H:%M:%S") + ' is ' + str(avgtraveltime) + ' seconds\n')
     begtime = endtime
     endtime = begtime + datetime.timedelta(0,300)
 
 analysis_end_time = time.time()
 print("it took %s seconds to analyze data for query 3" % (analysis_end_time - extract_end_time))
-
+query3_results.write("it took %s seconds to analyze data for query 3\n" % (analysis_end_time - extract_end_time))
 
 pool.dispose()
 print('all done\n')
+query3_results.write("all done\n")
+query3_results.close()
