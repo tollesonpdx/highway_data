06426a130505f5cba499cc4c6f6a387e6ee4d467
Chad Tolleson <tolleson@pdx.edu>
2019-12-10 15:35:32 -0800

    added times file

--- team_cassandra/queries/query3.py
+++ team_cassandra/queries/query3.py
@@ -6,7 +6,7 @@ import csv
 import time
 import datetime
 
-pool = ConnectionPool('highwaydata', ['10.138.0.5', '10.138.0.4', '10.138.0.3'], use_threadlocal=False, pool_size=3)
+pool = ConnectionPool('highwaydata', ['10.138.0.5', '10.138.0.4', '10.138.0.3'], use_threadlocal=False, pool_size=12)
 
 
 query3_start_time = time.time()
