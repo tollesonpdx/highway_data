addb23f78c13983819f872ee9a9e8a0f6bfc451f
Chad Tolleson <tolleson@pdx.edu>
2019-12-10 07:51:00 -0800

    added testing files

--- team_cassandra/queries/query3.py
+++ team_cassandra/queries/query3.py
@@ -33,7 +33,8 @@ stat_expr = create_index_expression('stationid', 1047)
 clause = create_index_clause([stat_expr])
 for key, row in detector_col_fam.get_indexed_slices(clause):
     temp_dets.append(row)
-print("temp dets are: " + temp_dets)
+print("temp dets are: ")
+print(temp_dets)
 
 
 loops = []
